5ffe6a045ce990fc24519539c684ef77c7c87258
paki-valdi-9 <palkourbanek45@gmail.com>
2025-10-12 00:45:57 +0200

    chore: correct/align name of the project

--- jest.config.ts
+++ jest.config.ts
@@ -91,7 +91,7 @@ const config: Config = {
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
-    '@personal-quiz-app-f/(.*)': '<rootDir>/src/$1',
+    '@customer_filter/(.*)': '<rootDir>/src/$1',
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
