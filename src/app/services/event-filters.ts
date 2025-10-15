453804eee261f2aed9a3c7f75a9351887f101e18
paki-valdi-9 <palkourbanek45@gmail.com>
2025-10-15 22:35:16 +0200

    feat: filters for event and attribute selectors with adjustment for fetching data in component

--- src/app/services/event-filters.ts
+++ src/app/services/event-filters.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
+import { BehaviorSubject, Observable, filter, map } from 'rxjs';
 import { CustomerEventDto, EventsDto, PropertyDto } from './models';
 import { CustomerEventDomain } from '../filter-panel/store/model';
 import { v4 as uuidv4 } from 'uuid';
@@ -12,19 +12,31 @@ export class EventService {
   private url = 'https://br-fe-assignment.github.io/customer-events/events.json';
   private readonly http = inject(HttpClient);
 
+  // reusing for initial load for filter in mat-select
+  private readonly _events$ = new BehaviorSubject<CustomerEventDomain[] | null>(null);
+  readonly events$ = this._events$
+    .asObservable()
+    .pipe(filter((customerEvent): customerEvent is CustomerEventDomain[] => !!customerEvent));
+
   toDomainEvents(): Observable<CustomerEventDomain[]> {
-    return this.http.get<EventsDto>(this.url).pipe(
-      map((fetchedData) => {
-        return (fetchedData.events || []).map((event: CustomerEventDto) => ({
-          id: uuidv4(),
-          name: event.type,
-          properties: event.properties.map((property: PropertyDto) => ({
-            id: uuidv4(),
-            type: property.type,
-            name: property.property,
-          })),
-        }));
-      })
-    );
+    if (!this._events$.value) {
+      this.http
+        .get<EventsDto>(this.url)
+        .pipe(
+          map((fetchedData) =>
+            (fetchedData.events || []).map((event: CustomerEventDto) => ({
+              id: uuidv4(),
+              name: event.type,
+              properties: event.properties.map((property: PropertyDto) => ({
+                id: uuidv4(),
+                type: property.type,
+                name: property.property,
+              })),
+            }))
+          )
+        )
+        .subscribe((events) => this._events$.next(events));
+    }
+    return this.events$;
   }
 }
